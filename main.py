fc7e0316ffcf1a7f661c906ba6c98f9cb283bdb9
Thiago Souza <thiagossouza07@gmail.com>
2022-02-06 16:09:34 -0300

    Implementando Sistema de Pontos

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
 total_de_chutes = 0
+pontos = 1000
 
 print("Qual o nível de dificuldade?")
 print("(1) - Fácil, (2) - Médio, (3) - Difícil")
@@ -32,13 +33,15 @@ for rodada in range(1, total_de_chutes + 1):
     menor = chute < numero_secreto
 
     if(acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto!")
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto!")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 
 print("Fim do Jogo!")
