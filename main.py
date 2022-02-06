0dbd0db965d5c9576e280760044e1a4946339d21
Thiago Souza <thiagossouza07@gmail.com>
2022-02-06 15:40:19 -0300

    Implementando o laço com For

--- main.py
+++ main.py
@@ -6,23 +6,27 @@ numero_secreto = 42
 total_de_chutes = 3
 rodada = 1
 
-while(rodada <= total_de_chutes):
+for rodada in range(1, total_de_chutes + 1):
     print("Tentativa {} de {}".format(rodada, total_de_chutes))
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
     print("Você digitou: ", chute)
 
+    if(chute < 1 or chute >100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
     acertou = numero_secreto == chute
     maior = chute > numero_secreto
     menor = chute < numero_secreto
 
     if(acertou):
         print("Você acertou!")
+        break
     else:
         if(maior):
             print("Você errou! O seu chute foi maior que o número secreto!")
         elif(menor):
             print("Você errou! O seu chute foi menor que o número secreto!")
-    rodada = rodada + 1
+
 
 print("Fim do Jogo!")
 
